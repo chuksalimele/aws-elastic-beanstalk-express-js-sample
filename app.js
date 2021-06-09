a6fbd6c99d2a331a79d39e0af7c1a342597038b4
Chuks Alimele <chuksalimele@gmail.com>
2021-06-09 05:41:54 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('MY FIRST AWS TUTORIAL!'));
+app.get('/', (req, res) => res.send('MY FIRST AWS TUTORIAL STILL IN PROGRESS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
